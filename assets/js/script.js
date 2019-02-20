3febe33ba35704611e27c1b693aae69d19f5c8c5
Willas Mar <willasanime@yahoo.com.br>
2019-02-20 08:56:10 -0300

    Alteração no projeto, cursor do mouse

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,4 +10,19 @@ $(function(){
 			$('#vez').html('<img src="assets/img/X.png" width="70">');
 		}		
 	}
+
+	/* ao setar a imagem no ponteiro do mouse usa-se esse caminho
+	por que ele inserido no arquivo html, e esse arquivo fica antes da
+	pasta assets. */
+	$('.areaInt').bind('mousemove', function(){
+		if($(this).find('img').length == 0){
+			if(vez == 0){
+				$('.areaInt').css('cursor','url("assets/img/O_64.ico"), default');
+			}else{
+				$('.areaInt').css('cursor','url("assets/img/X_64.ico"), default');
+			}
+		}else{
+			$('.areaInt').css('cursor','auto');
+		}
+	});
 });
