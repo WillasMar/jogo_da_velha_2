c256a6fffe56ab41663404d7a11b8935ca975311
Willas Mar <willasanime@yahoo.com.br>
2019-02-21 07:57:18 -0300

    Alteração no projeto, evento de clique em .areaInt

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,18 +11,41 @@ $(function(){
 		}		
 	}
 
+	function verificarGanhador(){
+
+	}
+
 	/* ao setar a imagem no ponteiro do mouse usa-se esse caminho
 	por que ele inserido no arquivo html, e esse arquivo fica antes da
 	pasta assets. */
 	$('.areaInt').bind('mousemove', function(){
 		if($(this).find('img').length == 0){
 			if(vez == 0){
-				$('.areaInt').css('cursor','url("assets/img/O_64.ico"), default');
+				$(this).css('cursor','url("assets/img/O_64.ico"), default');
 			}else{
-				$('.areaInt').css('cursor','url("assets/img/X_64.ico"), default');
+				$(this).css('cursor','url("assets/img/X_64.ico"), default');
 			}
 		}else{
-			$('.areaInt').css('cursor','auto');
+			$(this).css('cursor','auto');
+		}
+	});
+
+	/* insere um evento de click para que adiciona uma tag img apontando para O.png ou X.png */
+	$('.areaInt').bind('click', function(){
+		if($(this).find('img').length == 0){
+			if(vez == 0){
+				$(this).html('<img src="assets/img/O.png" width="50">');
+				vez = 1;
+				$(this).css('cursor','auto');
+				atualizarVez();
+				verificarGanhador();
+			}else if(vez == 1){
+				$(this).html('<img src="assets/img/X.png" width="50">');
+				vez = 0;
+				$(this).css('cursor','auto');
+				atualizarVez();
+				verificarGanhador();
+			}
 		}
 	});
 });
